d8496c5813ac21c3a4fe743a160cadc1045571b5
Shanyi Liow <shanyiliow1107@gmail.com>
2026-03-14 06:39:32 +0800

    feat: implemented redirect feature based on the generated short_url

--- backend/app/controllers/short_urls_controller.rb
+++ backend/app/controllers/short_urls_controller.rb
@@ -3,4 +3,13 @@ class ShortUrlsController < ApplicationController
       url = ShortUrl.create(original_url: params[:long_url])
       render json: url, status: :created
     end
+
+    def redirect
+      url = ShortUrl.find_by!(short_url: params[:short_url])
+      
+      # return the long url and then redirect user to the origina url
+      render json: { longUrl: url.original_url }, status: :found, location: url.original_url
+    end
 end
+
+#TODO: Grab the Title tag from the target url and save it to db
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   post "/shorten", to: "short_urls#create"
+  get "/:short_url", to: "short_urls#redirect"
 
   # Defines the root path route ("/")
   # root "posts#index"
