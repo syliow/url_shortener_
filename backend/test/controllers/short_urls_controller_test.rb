f0c3f080d70e7ebbe2219a1ed54e31786fd1761f
Shanyi Liow <shanyiliow1107@gmail.com>
2026-03-17 21:09:23 +0800

    test: added test for short_urls_controller

--- backend/test/controllers/short_urls_controller_test.rb
+++ backend/test/controllers/short_urls_controller_test.rb
@@ -1,25 +1,52 @@
 require "test_helper"
 
 class ShortUrlsControllerTest < ActionDispatch::IntegrationTest
-  # Return a list of generated urls
+# Return a list of generated urls
   test "return a list of urls" do
-    
+    get urls_path
+    assert_response :success
+
+    json = JSON.parse(response.body)
+    assert_not_nil json["urls"]
+    assert_not_nil json["total"]
   end  
 
-  #test for pagination (it should return filtered result)
-  #return visit details for a specific short_url
-  #return visit count for the specific short_url
-  #create short_url before saving to db
-  #fetching title should work
-  #handle redirect for valid url
-  #tracking is working properly when user access redirect url
-  #handle redirect for invalid url
-  
-  #Some edge cases i can think of
-  #missing param for ipaddress, city or country
-  #user passed in int when create url (instead of string)
-  #missing longurl param when trying to create url/ longurl is invalid format
-  #user is trying to access a shorturl not found in db
-  #what would happen if fetchtitle failed
-  #what would happen if no response or super slow response from geocoder (3rd party)
+# Pagination
+  test "check for pagination" do
+    get urls_path, params: {page: 1, per_page: 50}
+    assert_response :success
+  end
+
+# Returns visit details for short url + count
+  test "return details for short_url visit" do
+    url = short_urls(:github)
+    get "/urls/#{url.short_url}"
+    assert_response :success
+  end
+
+# Test if short_url doesn't exist
+  test "returns error for invalid short_url" do
+    get "/urls/invalid123"
+    assert_response :error 
+  end
+
+# Creates short_url successfully (includes fetch_title working)
+  test "able to create short_url" do
+    post shorten_path, params: {long_url: "https://github.com"}
+    assert_response :created
+  end
+
+# Missing long_url, short_url param when creating
+  test "empty long_url param" do
+    post shorten_path, params: {}
+    assert_response :error 
+  end
+
+# Redirects valid url
+  test "redirect valid short_url" do
+    url = short_urls(:github)
+    get "/#{url.short_url}"
+    assert_response :found
+  end
+
 end
