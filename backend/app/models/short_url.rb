c0783bf1024928e202c66904263a360ad90c1452
Shanyi Liow <shanyiliow1107@gmail.com>
2026-03-18 11:51:29 +0800

    refactor: replace magic numbers with constants

--- backend/app/models/short_url.rb
+++ backend/app/models/short_url.rb
@@ -6,18 +6,20 @@ class ShortUrl < ApplicationRecord
 
     # Base62 instead of random string
     CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars
+    BASE62 = 62
+    ID_OFFSET = 1000000000
 
     private
     def generate_short_url
         # We start at 1 billion to ensure a 6-digit look 
-        number = self.id + 1_000_000_000
+        number = self.id + ID_OFFSET
         code = ""
 
         # Divide ID by 62 and remainders to CHARACTERS until number is 0
         while number > 0
-            remainder = number % 62
+            remainder = number % BASE62
             code = CHARACTERS[remainder] + code
-            number /= 62
+            number /= BASE62
         end
 
         self.update(short_url: code)
